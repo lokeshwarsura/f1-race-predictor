bad269d5fd0d6a9855d1d56c0a5d4f1c64187919
lokeshwarsura <lokeshwarsura@gmail.com>
2026-05-11 21:17:06 +0530

    styled driver card

--- f1-react-dashboard/src/components/DriverCard.jsx
+++ f1-react-dashboard/src/components/DriverCard.jsx
@@ -1,30 +1,37 @@
-function DriverCard({
-
-  driver,
-
-  team,
-
-  image,
-
-  logo
-
-}) {
+function DriverCard({ driver, team, image, logo }) {
 
   return (
 
-    <div className="driver-card">
+    <div
+      style={{
+        background: '#111',
+        border: '2px solid red',
+        borderRadius: '20px',
+        padding: '25px',
+        width: '320px',
+        textAlign: 'center',
+        color: 'white',
+        boxShadow: '0 0 20px rgba(255,0,0,0.5)'
+      }}
+    >
 
       <img
-
         src={image}
-
         alt={driver}
-
-        className="driver-image"
-
+        style={{
+          width: '220px',
+          height: '220px',
+          objectFit: 'cover',
+          borderRadius: '15px'
+        }}
       />
 
-      <h2>
+      <h2
+        style={{
+          marginTop: '15px',
+          color: 'yellow'
+        }}
+      >
         {driver}
       </h2>
 
@@ -33,13 +40,15 @@ function DriverCard({
       </h3>
 
       <img
-
         src={logo}
-
         alt={team}
-
-        className="team-logo"
-
+        style={{
+          width: '140px',
+          marginTop: '20px',
+          borderRadius: '10px',
+          background: 'white',
+          padding: '10px'
+        }}
       />
 
     </div>
